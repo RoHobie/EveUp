07a6d16ca0099d70b8bc96e05e5efa9cbf4c530c
rohobie <ronitthakur267@gmail.com>
2026-03-18 03:06:50 +0530

    Add equals and hashcode methods temporarily to prevent StackOverFlow

--- src/main/java/com/eveup/tickets/domain/QRCode.java
+++ src/main/java/com/eveup/tickets/domain/QRCode.java
@@ -6,6 +6,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 import java.util.UUID;
 
 @Entity
@@ -41,4 +42,15 @@ public class QRCode {
     @Column(name = "updated_at", updatable = true, nullable = false)
     private LocalDateTime updatedAt;
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        QRCode qrCode = (QRCode) o;
+        return Objects.equals(id, qrCode.id) && status == qrCode.status && Objects.equals(value, qrCode.value) && Objects.equals(createdAt, qrCode.createdAt) && Objects.equals(updatedAt, qrCode.updatedAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, status, value, createdAt, updatedAt);
+    }
 }
--- src/main/java/com/eveup/tickets/domain/Ticket.java
+++ src/main/java/com/eveup/tickets/domain/Ticket.java
@@ -8,6 +8,7 @@ import org.springframework.data.annotation.LastModifiedDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 @Entity
@@ -23,6 +24,7 @@ public class Ticket {
     @Column(name = "id", nullable = false, updatable = false)
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
 
     @Column(name = "status", nullable = false)
     @Enumerated(EnumType.STRING)
@@ -49,5 +51,17 @@ public class Ticket {
     @LastModifiedDate
     @Column(name = "updated_at", updatable = true, nullable = false)
     private LocalDateTime updatedAt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Ticket ticket = (Ticket) o;
+        return Objects.equals(id, ticket.id) && status == ticket.status && Objects.equals(createdAt, ticket.createdAt) && Objects.equals(updatedAt, ticket.updatedAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, status, createdAt, updatedAt);
+    }
 
 }
