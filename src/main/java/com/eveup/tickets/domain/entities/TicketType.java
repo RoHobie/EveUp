5c7c2f8a6fe6710d411a023d19ba79ad3970f994
rohobie <ronitthakur267@gmail.com>
2026-03-19 00:17:22 +0530

    add missing name field

--- src/main/java/com/eveup/tickets/domain/entities/TicketType.java
+++ src/main/java/com/eveup/tickets/domain/entities/TicketType.java
@@ -26,6 +26,9 @@ public class TicketType {
     private UUID id;
 
     @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "price", nullable = false)
     private Double price;
 
     @Column(name = "description")
@@ -54,11 +57,11 @@ public class TicketType {
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         TicketType that = (TicketType) o;
-        return Objects.equals(id, that.id) && Objects.equals(price, that.price) && Objects.equals(description, that.description) && Objects.equals(totalAvailable, that.totalAvailable) && Objects.equals(createdAt, that.createdAt) && Objects.equals(updatedAt, that.updatedAt);
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && Objects.equals(price, that.price) && Objects.equals(description, that.description) && Objects.equals(totalAvailable, that.totalAvailable) && Objects.equals(createdAt, that.createdAt) && Objects.equals(updatedAt, that.updatedAt);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, price, description, totalAvailable, createdAt, updatedAt);
+        return Objects.hash(id, name, price, description, totalAvailable, createdAt, updatedAt);
     }
 }
